ba4d9c766ee63e243d2f130b01166311f4cff7cc
jimmischeff <jimmischeff@gmail.com>
2020-10-30 10:25:07 -0400

    added dotenv require

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require('cors')
+require("dotenv").config();
 const PORT = { PORT = 4500, NODE_ENV = 'development' } = process.env;
 
 
